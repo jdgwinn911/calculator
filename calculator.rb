9df9688ee73441ac9ec12c32b05862a6de90f0cb
joshua Gwinn <jdgwinn911@gmail.com>
2018-06-13 01:14:12 -0400

    i made it shorter

--- calculator.rb
+++ calculator.rb
@@ -1,29 +1,14 @@
-
-puts " what numbers?"
-n1 = gets.chomp 
-
-puts "what numbers?"
- n2 = gets.chomp
-
-add = n1.to_i + n2.to_i 
-subt = n1.to_i - n2.to_i 
-mult = n1.to_i * n2.to_i 
-div = n1.to_i / n2.to_i
-
-order = [add, subt, mult, div]
-
-puts "what do you want to do with them?"
-popo = gets.chomp 
-if popo == "add"
-    order[0]
-    puts add 
-elsif popo == "subtract"
-    order[1]
-    puts subt
-elsif popo == "multiply"
-    order[2]
-    puts mult
-elsif popo == "divide"
-    order[3]
-    puts div
-end
+puts "enter two sets of numbers"; n1 = gets.chomp.to_i
+    n2 = gets.chomp.to_i;  puts "what do you want to do with the numbers?"
+    add = n1 + n2; subtract = n1 - n2; multiply = n1 * n2; divide = n1 / n2
+    order = [add, subtract, multiply, divide]
+    calculation = gets.chomp
+    if (n1 =~ /[a-z]/) || (n2 =~ /[a-z]/)    # =~ /[a-z]/  means any letter from a to z i guess like a range? lol
+        puts "no letters allowed!"
+    else 
+        if calculation == "add"; puts order[0]; elsif calculation == "subtract"; puts order[1]
+            elsif calculation == "multiply"; puts order[2]; elsif calculation == "divide"; puts order[3]
+                if (n1 == 0) || (n2 == 0); puts " you can't do that ya fool!"
+                end
+            end
+        end
